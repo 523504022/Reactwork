9e3f832fa46620a6f05de645bec40829b7d530d3
Tuz1 <523504022@qq.com>
2019-01-13 20:20:59 +0800

    1

--- hello-react01/src/App.jsx
+++ hello-react01/src/App.jsx
@@ -1,13 +1,39 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import {NavLink,Route,Switch} from 'react-router-dom'
 
+import About from './views/about'
+import Home from './views/home'
 export default class App extends Component {
   render() {
     return (
       <div>
-        <img src={logo} alt="logo" className='logo'/>
-        <h2>first react demo</h2>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header"><h2>React Router Demo</h2></div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              {/*定义路由链接*/}
+              <NavLink className='list-group-item' to='/about'>About</NavLink>
+              <NavLink className='list-group-item' to='/home'>Home</NavLink>
+            </div>
+          </div>
+
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                {/*在此定义所有在此显示的路由*/}
+                <Switch>
+                  <Route path='/about' component={About}/>
+                  <Route path='/home' component={Home}/>
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
